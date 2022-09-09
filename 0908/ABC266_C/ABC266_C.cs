0b7efe5a5729e06f69d51712af037d9b0ef3f789
goshima <goshima.k.ab@gmail.com>
2022-09-09 15:44:57 +0900

    ABC266_C

--- 0908/ABC266_C/ABC266_C.cs
+++ 0908/ABC266_C/ABC266_C.cs
@@ -13,21 +13,22 @@ namespace ABC266C
             int[] C = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[] D = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[][] side = {A, B, C, D};
+            var calc = new Program();
             
-            if (calc_1(side[1][0], side[1][1], side) == calc_1(side[3][0], side[3][1], side))
+            if (calc.calc_1(side[1][0], side[1][1], side) == calc.calc_1(side[3][0], side[3][1], side))
             {
                 Console.WriteLine("No");
             }
             else
             {
-                if (calc_2(side[0][0], side[0][1], side) == calc_2(side[2][0], side[2][1], side))
+                if (calc.calc_2(side[0][0], side[0][1], side) == calc.calc_2(side[2][0], side[2][1], side))
                     Console.WriteLine("No");
                 else
                     Console.WriteLine("Yes");
             }
         }
 
-        static int calc_1(int x, int y, int[][] side)
+        int calc_1(int x, int y, int[][] side)
         {
             float a = (side[2][0] - side[0][0]) * (y - side[0][1]) - (side[2][1] - side[0][1]) * (x - side[0][0]);
             if (a > 0)
@@ -36,7 +37,7 @@ namespace ABC266C
                 return (-1);
         }
 
-        static int calc_2(int x, int y, int[][] side)
+        int calc_2(int x, int y, int[][] side)
         {
             float a = (side[3][0] - side[1][0]) * (y - side[1][1]) - (side[3][1] - side[1][1]) * (x - side[1][0]);
             if (a > 0)
